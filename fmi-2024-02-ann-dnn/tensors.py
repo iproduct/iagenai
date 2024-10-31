543d93cad6fb43c9a221b57fe0b597c2d35d856e
Trayan Iliev <office@iproduct.org>
2024-10-31 21:40:56 +0200

    examples updated to new version of libraries

--- fmi-2024-02-ann-dnn/tensors.py
+++ fmi-2024-02-ann-dnn/tensors.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from keras.datasets import mnist
-from keras.datasets import mnist
+from keras.api.datasets import mnist
+from keras.api.datasets import mnist
 
 if __name__ == '__main__':
     t = np.array(12)
