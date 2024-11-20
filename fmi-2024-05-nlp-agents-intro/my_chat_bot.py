8946ac6df7d50e75fec8a4deee5ebcc386b38320
Trayan Iliev <office@iproduct.org>
2024-11-20 14:50:06 +0200

    maya chat bot demo updated

--- fmi-2024-05-nlp-agents-intro/my_chat_bot.py
+++ fmi-2024-05-nlp-agents-intro/my_chat_bot.py
@@ -35,31 +35,55 @@ class ChatBot():
         play("response.mp3")
         os.remove("response.mp3")
 
+    def what(self, text):
+        lst = ["what are you", "who are you"]
+        return True if any(i in text.lower() for i in lst) else False
+
     def run(self):
         text= ''
         while text is None or len(text.strip()) == 0 or ('quit' not in text.strip().lower()):
             text = self.speech_to_text()
+
+
+
             if text is not None:
-                resp = ollama.generate(model=self.model, prompt=text)['response']
-                first_sentence = resp.split('\n')[0]
+                ## what
+                if self.what(text) is True:
+                    first_sentence = "I am an AI created by Trayan"
+                else:
+                    resp = ollama.generate(model=self.model, prompt=text)['response']
+                    first_sentence = resp.split('\n')[0]
                 print("AI -> ", first_sentence)
                 maya.text_to_speech(first_sentence)
         print(text)
 
+
 if __name__ == "__main__":
 
     # ChatBot demo
-    maya = ChatBot('Maya', 'llama3.2')
+    maya = ChatBot('Maya', 'maya')
     maya.run()
     # text = maya.speech_to_text()
     # if text is not None:
     #     maya.text_to_speech(text)
 
     # Speech generation using gTTS
-    # text = "I'm Maya. Do you want to chat?"
-    # text = "Аз съм Мая. Искаш ли да чатим?"
+    # resp = ollama.chat(model='llama3.1', messages=[
+    #     {
+    #         'role': 'system',
+    #         'content': 'You are an AI assistant.',
+    #     },
+    #     {
+    #         'role': 'user',
+    #         'content': 'Why is the sky blue?',
+    #     },
+    # ])
+    # text = resp['message']['content']
+    # print(text)
+    # # text = "I'm Maya. Do you want to chat?"
+    # # text = "Аз съм Мая. Искаш ли да чатим?"
     # print("AI -> ", text)
-    # speaker = gTTS(text=text, lang='bg', slow=False)
+    # speaker = gTTS(text=text, lang='en', slow=False)
     # speaker.save("response.mp3")
     # # os.system("start response.mp3") #linux -> aplay, mpg123 -q response.mp3; #macos -> afplay; #windows -> start""
     # play("response.mp3")
