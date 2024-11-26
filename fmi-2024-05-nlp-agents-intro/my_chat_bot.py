1f27bd598233ac0ff4138113a4275fe01af88497
Trayan Iliev <office@iproduct.org>
2024-11-26 23:20:58 +0200

    ollama.generate() -> ollama.chat()

--- fmi-2024-05-nlp-agents-intro/my_chat_bot.py
+++ fmi-2024-05-nlp-agents-intro/my_chat_bot.py
@@ -5,13 +5,13 @@ from gtts import gTTS
 import speech_recognition as sr
 from play_windows import play
 
+
 class ChatBot():
     def __init__(self, name, model):
         print("--- Starting up ", name, "---")
         self.name = name
         self.model = model
         self.recognizer = sr.Recognizer()
-
 
     # Speech recognition using SpeechRecognition
     def speech_to_text(self):
@@ -35,34 +35,50 @@ class ChatBot():
         play("response.mp3")
         os.remove("response.mp3")
 
+    def quit(self, text):
+        lst = ["quit", "finish", "exit", "bye"]
+        return True if any(i in text.lower() for i in lst) else False
+
     def what(self, text):
         lst = ["what are you", "who are you"]
         return True if any(i in text.lower() for i in lst) else False
 
     def run(self):
-        text= ''
-        while text is None or len(text.strip()) == 0 or ('quit' not in text.strip().lower()):
+        text = ''
+        messages = []
+        while text is None or len(text.strip()) == 0 or not self.quit(text):
             text = self.speech_to_text()
-
-
 
             if text is not None:
                 ## what
                 if self.what(text) is True:
                     first_sentence = "I am an AI created by Trayan"
                 else:
-                    resp = ollama.generate(model=self.model, prompt=text)['response']
-                    first_sentence = resp.split('\n')[0]
-                print("AI -> ", first_sentence)
-                maya.text_to_speech(first_sentence)
-        print(text)
+                    # resp = ollama.generate(model=self.model, prompt=text)['response']
+                    messages.append({
+                        'role': 'user',
+                        'content': text
+                    })
+                    resp = ollama.chat(model=self.model, messages=[
+                        {
+                            'role': 'system',
+                            'content': 'You are an AI assistant. Your name is Sophia. You are a philosopher nerd girl with a curiosity and experimenter attitude towards life. You also have sexy humor and like to make conversation entertaining and fun. You keep your answers short (no more than two sentences long), and up to the point.',
+                        }
+                    ] + messages)['message']['content']
+                    messages.append({
+                        'role': 'system',
+                        'content': resp
+                    })
+                    first_sentence = resp.split('\n')[:2]
+                    print("AI -> ", first_sentence)
+                    self.text_to_speech("\n".join(first_sentence))
+                    # print(text)
 
 
 if __name__ == "__main__":
-
     # ChatBot demo
-    maya = ChatBot('Maya', 'maya')
-    maya.run()
+    sophia = ChatBot('Sophia', 'llama3.2')
+    sophia.run()
     # text = maya.speech_to_text()
     # if text is not None:
     #     maya.text_to_speech(text)
@@ -100,4 +116,3 @@ if __name__ == "__main__":
     #    print("You -> ", text)
     # except Exception as e:
     #     print("You -> Recognition Error", e.message)
-
