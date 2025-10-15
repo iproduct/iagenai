90e577560c5a443b284007a3630c3cdf40828d33
Trayan Iliev <office@iproduct.org>
2025-10-15 21:17:58 +0300

    langchain  sql tools complete

--- fmi-2025-02-agents/agents_code.py
+++ fmi-2025-02-agents/agents_code.py
@@ -1,9 +1,19 @@
 import pandas as pd
+from langchain_core.tools import tool
 from langchain_ollama import OllamaLLM
 import sqlite3
 from langchain_community.tools import ListSQLDatabaseTool, InfoSQLDatabaseTool, QuerySQLDatabaseTool
+# from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
+# from langchain_community.tools.sql_database.tool import (
+#     InfoSQLDatabaseTool,
+#     ListSQLDatabaseTool,
+#     QuerySQLCheckerTool,
+#     QuerySQLDatabaseTool,
+# )
 
-from pandas.io.sql import SQLDatabase
+# toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+
+from langchain_community.utilities.sql_database import SQLDatabase
 
 if __name__ == '__main__':
     dtf = pd.read_csv("data_titanic.csv")
@@ -22,7 +32,28 @@ if __name__ == '__main__':
     # create sqlite db from Titanic dataset
     dtf.to_sql(index = False, name='titanic', con=sqlite3.connect("titanic.db"), if_exists='replace')
 
-    db = SQLDatabase('sqlite:///titanic.db')
+    db = SQLDatabase.from_uri("sqlite:///titanic.db")
+    @tool("tool_tables")
+    def tool_tables() -> str:
+        """Get all tables in the database"""
+        return ListSQLDatabaseTool(db=db).invoke('')
+
+    @tool("tool_schema")
+    def tool_schema(tables: str) -> str:
+        """Get tables schema. Example input: table1, table2, table3"""
+        return InfoSQLDatabaseTool(db=db).invoke(tables)
+
+    @tool("tool_query")
+    def tool_query(sql: str) -> str:
+        """Execute a SQL query"""
+        return QuerySQLDatabaseTool(db=db).invoke(sql)
 
 
+    print('--- Get tables: ---')
+    print(tool_tables.run(''))
 
+    print('--- Get schema: ---')
+    print(tool_schema.run(tool_tables.run('')))
+
+    print('--- SQL Query: ---')
+    print(tool_query.run(f'SELECT * FROM {tool_tables.run('')} LIMIT 3'))
