679e408f021d28c08068a9d9fb90fc337f7a6454
Trayan Iliev <office@iproduct.org>
2025-10-15 22:28:25 +0300

    crewai DB Engineer agent using qwen3 llm complete, prompt adjusted for qwen3

--- fmi-2025-02-agents/agents_code.py
+++ fmi-2025-02-agents/agents_code.py
@@ -1,8 +1,11 @@
+import crewai
 import pandas as pd
-from langchain_core.tools import tool
+from crewai import LLM
+from crewai.tools import tool
 from langchain_ollama import OllamaLLM
 import sqlite3
-from langchain_community.tools import ListSQLDatabaseTool, InfoSQLDatabaseTool, QuerySQLDatabaseTool
+from langchain_community.tools import ListSQLDatabaseTool, InfoSQLDatabaseTool, QuerySQLDatabaseTool, QuerySQLCheckerTool
+
 # from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 # from langchain_community.tools.sql_database.tool import (
 #     InfoSQLDatabaseTool,
@@ -10,7 +13,6 @@ from langchain_community.tools import ListSQLDatabaseTool, InfoSQLDatabaseTool, 
 #     QuerySQLCheckerTool,
 #     QuerySQLDatabaseTool,
 # )
-
 # toolkit = SQLDatabaseToolkit(db=db, llm=llm)
 
 from langchain_community.utilities.sql_database import SQLDatabase
@@ -19,6 +21,8 @@ if __name__ == '__main__':
     dtf = pd.read_csv("data_titanic.csv")
     print(dtf.head(3))
     llm = OllamaLLM(model="qwen3:8b")
+    # crewai_llm = LLM(model="ollama/codestral:latest ")
+    crewai_llm = LLM(model="ollama/qwen3:8b")
 
     print(f'How many passengers in Titanic: {len(dtf)}')
     print(f'How many passengers survived: {len(dtf[dtf["Survived"] == 1])}')
@@ -45,15 +49,61 @@ if __name__ == '__main__':
 
     @tool("tool_query")
     def tool_query(sql: str) -> str:
-        """Execute a SQL query"""
+        """Execute a SQL query and return the result"""
         return QuerySQLDatabaseTool(db=db).invoke(sql)
 
 
+    @tool("tool_check")
+    def tool_check(sql: str) -> str:
+        """
+        Before executing a query, always use this tool to review the SQL query
+        and correct the code if necessary.
+        """
+        return QuerySQLCheckerTool(db=db, llm=llm).invoke({"query": sql})
+
+
+    # print(tool_check.run(f"SELECT * FROM {tool_tables.run()} LIMIT 3 WHRE id=5").split('\n')[0])
+
     print('--- Get tables: ---')
-    print(tool_tables.run(''))
+    print(tool_tables.run())
 
     print('--- Get schema: ---')
-    print(tool_schema.run(tool_tables.run('')))
+    print(tool_schema.run(tool_tables.run()))
 
     print('--- SQL Query: ---')
-    print(tool_query.run(f'SELECT * FROM {tool_tables.run('')} LIMIT 3'))
+    print(tool_query.run(f'SELECT * FROM {tool_tables.run()} LIMIT 3'))
+
+    prompt = '''Extract data with SQL query to answer {user_input}. 
+    IMPORTANT: ALWAYS use the `tool_query` BEFORE returning <final answer>. 
+    ONLY return <final answer> returned by `tool_query`.
+    '''
+
+    # DB Agent
+    agent_sql = crewai.Agent(
+        role='Database Engineer',
+        goal = prompt,
+        backstory = '''
+        You are an experienced database engineer that creates and optimize efficient SQL queries. 
+        Use the `tool_tables` to find tables.
+        Use the `tool_schema` to get the metadata for the tables.
+        Use the `tool_check` to review your queries before executing.
+        Use the `tool_query` to execute SQL queries and return the result.
+     ''',    tools=[tool_tables, tool_schema, tool_check, tool_query],
+    max_iter=10,
+    llm=crewai_llm,
+    allow_delegation=False,
+    verbose=True)
+
+## Task
+task_sql = crewai.Task(
+    description=prompt,
+    agent=agent_sql,
+    expected_output='''Output of the query'''
+)
+#%% md
+#### Test single Agent
+#%%
+crew = crewai.Crew(agents=[agent_sql], tasks=[task_sql], verbose=False)
+res = crew.kickoff(inputs={"user_input":"how many people died? Return a plain number."})
+# res = crew.kickoff(inputs={"user_input":"how many people survived?"})
+print("\nResponse:\n", res)
