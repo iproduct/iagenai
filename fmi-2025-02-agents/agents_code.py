e5d0a091323590ac9552ff0b182b87461bf71e23
Trayan Iliev <office@iproduct.org>
2025-10-15 20:51:45 +0300

    langchain  sql tools added

--- fmi-2025-02-agents/agents_code.py
+++ fmi-2025-02-agents/agents_code.py
@@ -1,4 +1,28 @@
 import pandas as pd
+from langchain_ollama import OllamaLLM
+import sqlite3
+from langchain_community.tools import ListSQLDatabaseTool, InfoSQLDatabaseTool, QuerySQLDatabaseTool
+
+from pandas.io.sql import SQLDatabase
 
 if __name__ == '__main__':
-    pd.read_csv("data_titanic.csv")
+    dtf = pd.read_csv("data_titanic.csv")
+    print(dtf.head(3))
+    llm = OllamaLLM(model="qwen3:8b")
+
+    print(f'How many passengers in Titanic: {len(dtf)}')
+    print(f'How many passengers survived: {len(dtf[dtf["Survived"] == 1])}')
+    print(f'How many passengers did not survive: {len(dtf[dtf["Survived"] == 0])}')
+
+    # res = llm.invoke('''
+    # Answer short with a single number:
+    # Do you know 'Titanic' dataset from Kaggle - If yes, tell me how many people survived.    ''')
+    # print(res)
+
+    # create sqlite db from Titanic dataset
+    dtf.to_sql(index = False, name='titanic', con=sqlite3.connect("titanic.db"), if_exists='replace')
+
+    db = SQLDatabase('sqlite:///titanic.db')
+
+
+
